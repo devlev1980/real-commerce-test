f2f089b4fc5d20e8b076838331e4aa8fd0f8eeee
Yevgeny Levin <devlev1980@gmail.com>
2021-01-20 23:36:44 +0200

    add refresh

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -17,21 +17,36 @@ export class HomeComponent implements OnInit {
   isShowPrimary: boolean = true;
   isShowGrid: boolean = true;
   searchTerm: string = '';
+  isRefresh: boolean = false;
 
 
   constructor(private api: ApiService, private render: Renderer2) {
   }
 
   ngOnInit(): void {
-    this.api.getResults().subscribe((data) => {
-      this.dataGroupedByType = _.chain(data)
-        .groupBy('Type')
-        .toPairs()
-        .map(pair => _.zipObject(['type', 'data'], pair))
-        .value();
-      console.log(this.dataGroupedByType);
+    this.getDataFromServer();
 
-    });
+  }
+
+  getDataFromServer(): void {
+    this.isRefresh = true;
+
+    setTimeout(() => {
+      this.isRefresh = true;
+      this.api.getResults().subscribe((data) => {
+        if (data) {
+          this.isRefresh = false;
+          this.dataGroupedByType = _.chain(data)
+            .groupBy('Type')
+            .toPairs()
+            .map(pair => _.zipObject(['type', 'data'], pair))
+            .value();
+          console.log(this.dataGroupedByType);
+        }
+
+
+      });
+    }, 1000);
   }
 
 
@@ -40,15 +55,14 @@ export class HomeComponent implements OnInit {
     return '';
   }
 
-  onToggleView(btnClass: string): void {
+  onToggleView(): void {
     this.isShowPrimary = !this.isShowPrimary;
     this.isShowGrid = !this.isShowGrid;
-    // if (btnClass === 'btn-primary') {
-    //   this.render.removeClass(this.toggleBtn.nativeElement, btnClass);
-    //   this.render.addClass(this.toggleBtn.nativeElement, 'btn-info');
-    // } else if (btnClass === 'btn-info') {
-    //   this.render.removeClass(this.toggleBtn.nativeElement, btnClass);
-    //   this.render.addClass(this.toggleBtn.nativeElement, 'btn-primary');
-    // }
   }
+
+  onClearSearch(): void {
+    this.searchTerm = '';
+  }
+
+
 }
